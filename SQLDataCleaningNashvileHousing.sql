ca1e5718b9deeb028afd38a748d07a6ba4cc61a4
Noah Bogatinoff <57084047+Noahbogatinoff@users.noreply.github.com>
2021-09-16 18:03:18 -0700

    Add files via upload
    initial commit

---
+++ SQLDataCleaningNashvileHousing.sql
@@ -0,0 +1,25 @@
+#Create housing table
+-- CREATE TABLE housing (
+-- 	id INT,
+--     parcel_id VARCHAR(255),
+--     acerage VARCHAR (255),
+--     land_use VARCHAR(255),
+--     property_address VARCHAR(255),
+--     sale_date VARCHAR(255),
+--     sale_price VARCHAR(255),
+--     legal_reference VARCHAR(255),
+--     sold_vacant VARCHAR(255),
+--     owner_name VARCHAR(255),
+--     owner_address VARCHAR(255),
+--     tax_dist VARCHAR(255),
+--     land_val VARCHAR(255),
+--     building_val VARCHAR(255),
+--     total_val VARCHAR(255),
+--     year_built VARCHAR(255),
+--     bedrooms VARCHAR(255),
+--     full_bath VARCHAR(255),
+--     half_bath VARCHAR(255))
+--     
+--     SELECT*
+--     FROM housing
+
